81e106038146b321c986592b1dd84df3361a6515
kt1313 <tomaklim@wp.pl>
2020-09-06 20:28:07 +0200

    dodane przyciski next i back

--- src/main/java/pl/javastart/fxbrowser/Main.java
+++ src/main/java/pl/javastart/fxbrowser/Main.java
@@ -4,7 +4,8 @@ import javafx.application.Application;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 import javafx.stage.Stage;
@@ -14,9 +15,15 @@ public class Main extends Application {
     @FXML
     private WebView webView;
 
+    @FXML
+    private Button backButton;
+
+    @FXML
+    private Button nextButton;
+
     @Override
     public void start(Stage stage) throws Exception {
-        AnchorPane mainPane = FXMLLoader.load(getClass().getResource("/fxml/browserPane.fxml"));
+        VBox mainPane = FXMLLoader.load(getClass().getResource("/fxml/browserPane.fxml"));
         Scene scene = new Scene(mainPane);
         stage.setScene(scene);
         stage.setTitle("Przeglądarka");
@@ -30,5 +37,7 @@ public class Main extends Application {
     public void initialize() {
         WebEngine engine = webView.getEngine();
         engine.load("https://google.pl");
+        backButton.setOnAction(x -> engine.executeScript("history.back();"));
+        nextButton.setOnAction(x -> engine.executeScript("history.forward();"));
     }
 }
